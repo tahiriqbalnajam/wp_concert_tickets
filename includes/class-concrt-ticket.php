42f2a9bcedcef406967a1c2dcab31d03ea079d42
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2023-09-20 15:11:12 +0500

    exopit_options

--- includes/class-concrt-ticket.php
+++ includes/class-concrt-ticket.php
@@ -1,71 +1,12 @@
 <?php
 
-/**
- * The file that defines the core plugin class
- *
- * A class definition that includes attributes and functions used across both the
- * public-facing side of the site and the admin area.
- *
- * @link       https://tinajam.wordpress.com/
- * @since      1.0.0
- *
- * @package    Concrt_Ticket
- * @subpackage Concrt_Ticket/includes
- */
 
-/**
- * The core plugin class.
- *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing site hooks.
- *
- * Also maintains the unique identifier of this plugin as well as the current
- * version of the plugin.
- *
- * @since      1.0.0
- * @package    Concrt_Ticket
- * @subpackage Concrt_Ticket/includes
- * @author     tahir iqbal <tahiriqbal09@gmail.com>
- */
 class Concrt_Ticket {
 
-	/**
-	 * The loader that's responsible for maintaining and registering all hooks that power
-	 * the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      Concrt_Ticket_Loader    $loader    Maintains and registers all hooks for the plugin.
-	 */
 	protected $loader;
-
-	/**
-	 * The unique identifier of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-	 */
 	protected $plugin_name;
-
-	/**
-	 * The current version of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      string    $version    The current version of the plugin.
-	 */
 	protected $version;
 
-	/**
-	 * Define the core functionality of the plugin.
-	 *
-	 * Set the plugin name and the plugin version that can be used throughout the plugin.
-	 * Load the dependencies, define the locale, and set the hooks for the admin area and
-	 * the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
 	public function __construct() {
 		if ( defined( 'CONCRT_TICKET_VERSION' ) ) {
 			$this->version = CONCRT_TICKET_VERSION;
@@ -81,60 +22,18 @@ class Concrt_Ticket {
 
 	}
 
-	/**
-	 * Load the required dependencies for this plugin.
-	 *
-	 * Include the following files that make up the plugin:
-	 *
-	 * - Concrt_Ticket_Loader. Orchestrates the hooks of the plugin.
-	 * - Concrt_Ticket_i18n. Defines internationalization functionality.
-	 * - Concrt_Ticket_Admin. Defines all hooks for the admin area.
-	 * - Concrt_Ticket_Public. Defines all hooks for the public side of the site.
-	 *
-	 * Create an instance of the loader which will be used to register the hooks
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function load_dependencies() {
 
-		/**
-		 * The class responsible for orchestrating the actions and filters of the
-		 * core plugin.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-concrt-ticket-loader.php';
-
-		/**
-		 * The class responsible for defining internationalization functionality
-		 * of the plugin.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-concrt-ticket-i18n.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the admin area.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-concrt-ticket-admin.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-concrt-ticket-public.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/exopite-simple-options/exopite-simple-options-framework-class.php';
 
 		$this->loader = new Concrt_Ticket_Loader();
 
 	}
 
-	/**
-	 * Define the locale for this plugin for internationalization.
-	 *
-	 * Uses the Concrt_Ticket_i18n class in order to set the domain and to register the hook
-	 * with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function set_locale() {
 
 		$plugin_i18n = new Concrt_Ticket_i18n();
@@ -143,13 +42,6 @@ class Concrt_Ticket {
 
 	}
 
-	/**
-	 * Register all of the hooks related to the admin area functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function define_admin_hooks() {
 
 		$plugin_admin = new Concrt_Ticket_Admin( $this->get_plugin_name(), $this->get_version() );
@@ -159,13 +51,6 @@ class Concrt_Ticket {
 
 	}
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
 	private function define_public_hooks() {
 
 		$plugin_public = new Concrt_Ticket_Public( $this->get_plugin_name(), $this->get_version() );
@@ -175,22 +60,10 @@ class Concrt_Ticket {
 
 	}
 
-	/**
-	 * Run the loader to execute all of the hooks with WordPress.
-	 *
-	 * @since    1.0.0
-	 */
 	public function run() {
 		$this->loader->run();
 	}
 
-	/**
-	 * The name of the plugin used to uniquely identify it within the context of
-	 * WordPress and to define internationalization functionality.
-	 *
-	 * @since     1.0.0
-	 * @return    string    The name of the plugin.
-	 */
 	public function get_plugin_name() {
 		return $this->plugin_name;
 	}
