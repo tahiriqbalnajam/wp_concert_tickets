6b4970d34d3f3f41ecc742b75b09e584023428a9
Tahir Iqbal Najam <tahiriqbal09@gmail.com>
2023-09-20 15:18:55 +0500

    shortcode

--- includes/class-concrt-ticket-loader.php
+++ includes/class-concrt-ticket-loader.php
@@ -31,15 +31,8 @@ class Concrt_Ticket_Loader {
 	 * @var      array    $actions    The actions registered with WordPress to fire when the plugin loads.
 	 */
 	protected $actions;
-
-	/**
-	 * The array of filters registered with WordPress.
-	 *
-	 * @since    1.0.0
-	 * @access   protected
-	 * @var      array    $filters    The filters registered with WordPress to fire when the plugin loads.
-	 */
 	protected $filters;
+	protected $shortcodes;
 
 	/**
 	 * Initialize the collections used to maintain the actions and filters.
@@ -79,6 +72,10 @@ class Concrt_Ticket_Loader {
 	 */
 	public function add_filter( $hook, $component, $callback, $priority = 10, $accepted_args = 1 ) {
 		$this->filters = $this->add( $this->filters, $hook, $component, $callback, $priority, $accepted_args );
+	}
+
+	public function add_shortcode( $tag, $component, $callback, $priority = 10, $accepted_args = 2 ) {
+    	$this->shortcodes = $this->add( $this->shortcodes, $tag, $component, $callback, $priority, $accepted_args );
 	}
 
 	/**
@@ -123,6 +120,10 @@ class Concrt_Ticket_Loader {
 		foreach ( $this->actions as $hook ) {
 			add_action( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
 		}
+
+		foreach ( $this->shortcodes as $hook ) {
+        	add_shortcode(  $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+    	}
 
 	}
 
